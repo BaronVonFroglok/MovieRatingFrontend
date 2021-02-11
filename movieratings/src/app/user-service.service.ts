85d040d9c3a0ef3ad2d416bd2a856c291e1e9fc4
JesseTWarren <jtwarrenz@gmail.com>
2021-02-11 16:05:01 -0500

    this be that user service, needs slight revision

--- movieratings/src/app/user-service.service.ts
+++ movieratings/src/app/user-service.service.ts
@@ -1,9 +1,27 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-
+import { User } from './user.models';
+import { Observable } from 'rxjs';
 @Injectable({
   providedIn: 'root'
 })
 export class UserServiceService {
+  private id: number = 0;
 
-  constructor() { }
+  /*PLEASE NOTE HARDCODED USER ID !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!*/
+  private getPortfoliosUrl: string = "http://localhost:8200/service/getPortfolios/";
+  //bug fix adding base url 1/1 
+  private baseUrl: string ="http://localhost:8200/service";
+
+  constructor(private http: HttpClient) {}
+  setId(id: number){
+    this.id = id;
+    }
+
+  
+  //bug fix team adding get user function 1/1
+  getUser(id: number): Observable<any> {
+    return this.http.get(`${this.baseUrl}/getUserById/${id}`);
+
+  }
 }
