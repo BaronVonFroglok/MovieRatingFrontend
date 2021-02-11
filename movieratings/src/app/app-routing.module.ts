bc01d6f44ab9f69feab1688e4536df7c4bea3f29
Stephanie Foshay <foshaystephanie@gmail.com>
2021-02-11 11:42:35 -0600

    basic html and bootstrap add for 3 components + routing

--- movieratings/src/app/app-routing.module.ts
+++ movieratings/src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LandingPageComponent } from './landing-page/landing-page.component';
+import { LoginComponent } from './login/login.component';
+import { ReviewComponent } from './review/review.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', pathMatch: 'full', redirectTo: 'login'},
+  {path: 'login', component: LoginComponent},
+  {path: 'landing-page', component: LandingPageComponent},
+  {path: 'review', component: ReviewComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- movieratings/src/app/app.module.ts
+++ movieratings/src/app/app.module.ts
@@ -3,16 +3,25 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { LandingPageComponent } from './landing-page/landing-page.component';
+import { ReviewComponent } from './review/review.component';
+import { HttpService } from './http.service';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    LandingPageComponent,
+    ReviewComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
-  providers: [],
+  providers: [HttpService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
